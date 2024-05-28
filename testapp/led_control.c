39b0b616aef280668faabbf1a05e497e9c786f8a
Elwin Hammer <elwinhamer@gmail.com>
2024-05-28 14:23:25 +0200

    Update led_control.c

--- testapp/led_control.c
+++ testapp/led_control.c
@@ -23,4 +23,17 @@ void write_led(const char *value) {
 int main(int argc, char **argv) {
     if (argc != 2) {
         fprintf(stderr, "Usage: %s <on|off>\n", argv[0]);
-        return
+        return 1;
+    }
+
+    if (strcmp(argv[1], "on") == 0) {
+        write_led("1");  // Assuming '1' turns the LED on
+    } else if (strcmp(argv[1], "off") == 0) {
+        write_led("0");  // Assuming '0' turns the LED off
+    } else {
+        fprintf(stderr, "Invalid argument. Use 'on' or 'off'.\n");
+        return 1;
+    }
+
+    return 0;
+}
