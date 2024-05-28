ad76663ca776acc0c240cbfd398ef6e55eaa1dd6
Elwin Hammer <elwinhamer@gmail.com>
2024-05-28 14:59:07 +0200

    Update test_sensor.c

--- arduino-i2c-sensor/testapp/test_sensor.c
+++ arduino-i2c-sensor/testapp/test_sensor.c
@@ -3,6 +3,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <string.h>
+#include <errno.h>  // Include this to use 'errno'
 
 int main() {
     int fd;
@@ -11,10 +12,14 @@ int main() {
     fd = open("/sys/class/i2c-dev/i2c-1/device/1-0008/distance", O_RDONLY);
     if (fd < 0) {
         perror("Failed to open the device");
-        return errno;
+        return errno;  // Now 'errno' is recognized
     }
 
-    read(fd, buf, sizeof(buf));
+    if (read(fd, buf, sizeof(buf)) < 0) {
+        perror("Failed to read from the device");
+        close(fd);
+        return errno;
+    }
     printf("Distance: %s cm\n", buf);
 
     close(fd);
